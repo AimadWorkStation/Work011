7933d1b76123f5b7a195f20f565f9565e1c6028f
EmadEddin <aimad.tchad@gmail.com>
2018-01-08 23:17:25 +0000

    #4 : adding some designe to the chat room

--- app/templates/chatroom.php
+++ app/templates/chatroom.php
@@ -2,10 +2,11 @@
 	
  ?>
 
- <div class="chatroom mt-5">
- 	<div class="row">
-		<div class="d-xl-none col-sm-12">
-			<div class="listefriendMobile d-block d-lg-none d-xl-block col-sm-12" style="background-color: red;height: 100px;">
+
+ 	<div class="row chatroom mt-5 mb-5 w-75 ml-auto mr-auto">
+
+		<div class="d-xl-none col-sm-12 listefriendMobile">
+			<div class="d-block d-lg-none d-xl-block col-sm-12" style="background-color: red;height: 100px;">
 	 			<div class="row chatheader" style="background-color: yellow; height: 80px;">
 		 			
 		 		</div>
@@ -16,22 +17,144 @@
 		</div>
 
 
- 		<div class="listefriend d-none d-lg-block col-lg-3" style="background-color: red;height: 400px;">
- 			<div class="row chatheader" style="background-color: yellow; height: 100px;">
-	 			
+ 		<div class="listefriend d-none d-lg-block col-lg-4">
+ 			<div class="row listheader">
+	 			<div class="search h-100 w-100 position-relative">
+	 				<input class="form-control ml-auto mr-auto w-75 mt-4 border-0 btn-outline-light" type="text" id="search" placeholder="Search" autocomplete="off">
+	 				<i class="fa fa-search"></i>
+	 			</div>
 	 		</div>
-	 		<div class="row chatbody">
-	 			
+	 		<div class="row listbody position-relative h-100" id="scrollstyle">
+	 			<ul class="list-group">
+				  <li class="row">
+		 				<div class="col-4">
+		 					<img src="app/img/user/default.jpg" class="img-fluid rounded-circle border border-secondary">
+		 				</div>
+		 				<div class="col-8 mt-4">
+		 					<span>EmadEddin</span>
+		 					<span><i class="fa fa-check-circle"> online</i></span>
+		 				</div>
+				  </li>
+
+				  <li class="row">
+		 				<div class="col-4">
+		 					<img src="app/img/user/default.jpg" class="img-fluid rounded-circle border border-secondary">
+		 				</div>
+		 				<div class="col-8 mt-4">
+		 					<span>EmadEddin</span>
+		 					<span><i class="fa fa-check-circle"> online</i></span>
+		 				</div>
+				  </li>
+
+				  <li class="row">
+		 				<div class="col-4">
+		 					<img src="app/img/user/default.jpg" class="img-fluid rounded-circle border border-secondary">
+		 				</div>
+		 				<div class="col-8 mt-4">
+		 					<span>EmadEddin</span>
+		 					<span><i class="fa fa-check-circle"> online</i></span>
+		 				</div>
+				  </li>
+
+				  <li class="row">
+		 				<div class="col-4">
+		 					<img src="app/img/user/default.jpg" class="img-fluid rounded-circle border border-secondary">
+		 				</div>
+		 				<div class="col-8 mt-4">
+		 					<span>EmadEddin</span>
+		 					<span><i class="fa fa-check-circle"> online</i></span>
+		 				</div>
+				  </li>
+
+				  <li class="row">
+		 				<div class="col-4">
+		 					<img src="app/img/user/default.jpg" class="img-fluid rounded-circle border border-secondary">
+		 				</div>
+		 				<div class="col-8 mt-4">
+		 					<span>EmadEddin</span>
+		 					<span><i class="fa fa-check-circle"> online</i></span>
+		 				</div>
+				  </li>
+
+				  <li class="row">
+		 				<div class="col-4">
+		 					<img src="app/img/user/default.jpg" class="img-fluid rounded-circle border border-secondary">
+		 				</div>
+		 				<div class="col-8 mt-4">
+		 					<span>EmadEddin</span>
+		 					<span><i class="fa fa-check-circle"> online</i></span>
+		 				</div>
+				  </li>
+
+				</ul>
 	 		</div>
 	 	</div>
-	 	<div class="chating col-sm-12 col-lg-8" style="background-color: green; height: 400px;">
-	 		<div class="row chatheader" style="background-color: blue; height: 100px;">
-	 			
+	 	<div class="chatingzone col-sm-12 col-lg-8 position-relative">
+	 		<div class="row chatheader" style="height: 100px;">
+	 			<div class="down col-8 row">
+	 				<div class="col-4">
+	 					<img src="app/img/user/default.jpg" class="img-fluid rounded-circle border border-secondary">
+	 				</div>
+	 				<div class="col-7 mt-4">
+	 					<span>Chat With EmadEddin</span>
+	 					<span>Messages 1213</span>
+	 				</div>
+	 			</div>
+	 			<div class="up col-4">
+	 				<span id="profil" class="float-right p-2">Profile</span>
+	 				<span id="logout" class="float-right p-2">  Logout</span>
+	 			</div>
 	 		</div>
-	 		<div class="row chatbody">
-	 			
+	 		<div class="row chatbody" id="scrollstyle">
+	 				<div class="w-100 text-center" id="showmore" style="height: 26px">
+	 					<span id="loadmore">Load more <i class="fa fa-spinner"></i></span>
+	 				</div>
+		 			<ul class="list-group w-100 position-relative list-unstyled h-100">
+						<li class="message-local">
+								Hello WorldHello
+						</li>
+						<li class="message-local">
+								Hello World
+						</li>	
+						<li class="message-local">
+								Hello World
+						</li>	
+						<li class="message-local">
+								Hello World
+						</li>	
+						<li class="message-remote">
+								Hello World
+						</li>	
+						<li class="message-local">
+								Hello World
+						</li>	
+						<li class="message-local">
+								Hello World
+						</li>	
+						<li class="message-local">
+								Hello World
+						</li>	
+						<li class="message-remote">
+								Hello World
+						</li>		
+						<li class="message-remote">
+								Hello World
+						</li>	
+						<li class="message-remote">
+								Hello World
+						</li>		  
+						<li class="message-remote">
+								Hello World
+						</li>	
+					</ul>
 	 		</div>
+	 		<div class="write position-absolute">
+				<input type="text" id="message" placeholder="message">
+				<span id="send">Send</span>
+				<span id="document"><i class="fa fa-file"></i></span>
+				<span id="photo"><i class="fa fa-photo"></i></span>
+			</div>
 	 	</div>
  	</div>
+
  	
- </div>
